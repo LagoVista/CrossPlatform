c665e692dbfba0bc34d70d70fbc1fcc25fbded44
Kevin D. Wolf <kevinw@software-logistics.com>
2021-07-19 14:01:23 -0400

    running on iOS

--- Apps/seawolf/SeaWolf/AppStyle.cs
+++ Apps/seawolf/SeaWolf/AppStyle.cs
@@ -12,7 +12,7 @@ namespace SeaWolf
         private readonly Color _medGray = Color.CreateColor(0x60, 0x60, 0x60);
         private readonly Color _lightGray = Color.CreateColor(0xA0, 0xA0, 0xA0);
 
-        private const string DefaultFont = "Roboto";
+        private const string DefaultFont = "Helvetica";
 
         public Color TitleBarBackground => NamedColors.NuvIoTDark;
 
