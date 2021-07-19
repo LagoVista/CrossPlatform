b870d77bbf6f17e47c54fcff9b53e5df3ae34aca
Kevin D. Wolf <kevinw@software-logistics.com>
2021-07-19 16:58:00 -0400

    update version

--- Apps/seawolf/SeaWolf.iOS/appcenter-post-clone.sh
+++ Apps/seawolf/SeaWolf.iOS/appcenter-post-clone.sh
@@ -14,7 +14,7 @@ iOSProjectName=SeaWolf.iOS
 appProjectName=SeaWolf.iOS
 
 # Version is pulled from a file in the root, it contains first two digits of full version, such as 1.0
-version=$(<$APPCENTER_SOURCE_DIRECTORY/version.txt)
+version=$(<$APPCENTER_SOURCE_DIRECTORY/App/seawolf/version.txt)
 
 # Revision is four digit number, always zero padded HHMM
 revision=`date +"%H%M"`
