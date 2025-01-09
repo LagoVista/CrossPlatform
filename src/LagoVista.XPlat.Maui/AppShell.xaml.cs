6640bca2bb28f4d454aa2c81d83db2430098c401
RyanCisewski <ryan.j.cisewski@gmail.com>
2025-01-09 09:25:27 -0600

    Add Maui SLN

---
+++ src/LagoVista.XPlat.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LagoVista.XPlat.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/LagoVista.XPlat.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LagoVista.XPlat.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
